26fb01ed0188978e7d9c4206dd45f5a25895605c
Tabatha Flores <tabatha.flores18@gmail.com>
2020-07-29 17:50:41 -0600

    breed service and repo and test

---
+++ CeboPets/src/main/java/com/skilldistillery/cebopets/services/BreedService.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cebopets.services;
+
+import java.util.List;
+
+import com.skilldistillery.cebopets.entities.Breed;
+
+public interface BreedService {
+	
+	Breed seeBreed(int breedId);
+	
+	List<Breed> allBreeds();
+
+}
